7950885adcd0d6a1a554bfb83c0eb7d35930a063
Jim Lloyd <jim.lloyd@gmail.com>
2014-12-09 14:07:46 -0800

    A partial implementation.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var _ = require('lodash');
-var Gremlin = require('gremlin-v3');
-var assert = require('assert');
+var assert = require('assert-plus');
 var fs = require('fs');
+var Gremlin = require('gremlin-v3');
+var Immutable = require('immutable');
+var mkdirp = require('mkdirp');
 
 var gremlin = new Gremlin();
 var java = gremlin.java;
@@ -13,8 +15,27 @@ function loadClass(className) {
   return java.getClassLoader().loadClassSync(className);
 }
 
+function shortClassName(className) {
+  var m = className.match(/\.([\$\w]+)$/);
+  assert.ok(m);
+  return m[1];
+}
+
+var done = Immutable.Set();
+var todo = Immutable.Set([
+  'com.tinkerpop.gremlin.structure.Graph'
+]);
+
+//   'com.tinkerpop.gremlin.process.graph.GraphTraversal',
+//   'com.tinkerpop.gremlin.process.graph.ElementTraversal',
+//   'com.tinkerpop.gremlin.structure.Vertex',
+//   'com.tinkerpop.gremlin.structure.Edge',
+//   'com.tinkerpop.gremlin.process.Traversal$SideEffects'
+
+var classes = {};
+
 function mapMethod(method) {
-  return {
+  var methodMap = {
     name: method.getNameSync(),
     declared: method.getDeclaringClassSync().getNameSync(),
     returns: method.getReturnTypeSync().getNameSync(),
@@ -23,16 +44,61 @@ function mapMethod(method) {
     generic: method.toGenericStringSync(),
     string: method.toStringSync(),
   };
+
+  function addIfNotQueued(className) {
+    if (className.match(/^com.tinkerpop.gremlin/)) {
+      if (!done.has(className) && !todo.has(className))
+        todo = todo.add(className);
+    }
+  }
+
+  addIfNotQueued(methodMap.declared);
+  addIfNotQueued(methodMap.returns);
+
+  return methodMap;
+}
+
+function mapClass(className) {
+  var clazz = loadClass(className);
+  assert.strictEqual(clazz.getNameSync(), className);
+
+  var shortName = shortClassName(className);
+  var methods = _.map(clazz.getMethodsSync(), mapMethod);
+
+  var classMap = {
+    fullName: className,
+    shortName: shortName,
+    methods: methods
+  };
+
+  return classMap;
 }
 
 function processClass(className) {
-  var clazz = loadClass(className);
-  var methods = _.map(clazz.getMethodsSync(), mapMethod);
-  fs.writeFileSync('out/' + className + '.txt', JSON.stringify(methods, null, '  '));
+  var classMap = mapClass(className);
+  var methods = classMap.methods;
+  var shortName = classMap.shortName;
+
+  var definedMethods = _.filter(methods, function (m) { return m.declared === className; });
+  var inheritedMethods = _.filter(methods, function (m) { return m.declared !== className; });
+  fs.writeFileSync('out/txt/' + shortName + '.defined.txt', JSON.stringify(definedMethods, null, '  '));
+  fs.writeFileSync('out/txt/' + shortName + '.inherited.txt', JSON.stringify(inheritedMethods, null, '  '));
 }
 
-processClass('com.tinkerpop.gremlin.process.graph.GraphTraversal');
-processClass('com.tinkerpop.gremlin.process.graph.ElementTraversal');
-processClass('com.tinkerpop.gremlin.structure.Vertex');
-processClass('com.tinkerpop.gremlin.structure.Edge');
-processClass('com.tinkerpop.gremlin.structure.Graph');
+function main() {
+
+  mkdirp.sync('out/txt');
+  mkdirp.sync('out/lib');
+  mkdirp.sync('out/test');
+
+  while (todo.size > 0) {
+    var className = todo.first();
+    todo = todo.remove(className);
+    done = done.add(className);
+    processClass(className);
+  }
+
+  console.log(done);
+}
+
+main();
