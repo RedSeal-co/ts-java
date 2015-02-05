26f2fc036fbe0687c03109050e7932952d25d357
Jim Lloyd <jim.lloyd@gmail.com>
2015-02-05 11:23:18 -0800

    Changes towards bootstrapping the java.d.ts used by ts-java.
    The file lib/java.d.ts was written by hand. I'd like to be able replace it
    with one generated by ts-java. This commit includes some manual changes
    to lib/java.d.ts to proactively reconcile some differences that will be
    introduced when we do use a ts-java generated java.d.ts file.
    
    As part of those differences, I need to add a few more required classes,
    which means some minor changes to classes-map.ts and classes-map-test.ts.

--- lib/classes-map.ts
+++ lib/classes-map.ts
@@ -12,11 +12,14 @@ import Work = require('./work');
 
 var requiredSeedClasses = [
   'java.lang.Class',
+  'java.lang.ClassLoader',
   'java.lang.Cloneable',
   'java.lang.Comparable',
   'java.lang.Enum',
   'java.lang.Iterable',
   'java.lang.Long',
+  'java.lang.reflect.Method',
+  'java.lang.reflect.Constructor',
   'java.lang.Number',
   'java.lang.Object',
   'java.lang.String',
